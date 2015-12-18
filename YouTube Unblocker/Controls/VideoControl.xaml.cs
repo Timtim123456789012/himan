4a5e471075127a9368dc169a26d3edaff0df0aed
Syrexide <wwc.302@gmail.com>
2015-12-17 21:17:20 -0500

    Initial release

---
+++ YouTube Unblocker/Controls/VideoControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace YouTube_Unblocker.Controls
+{
+    /// <summary>
+    /// Interaction logic for SettingsControl.xaml
+    /// </summary>
+    public partial class VideoControl : UserControl
+    {
+        public readonly MainWindow _mainWindow;
+        public static readonly Regex youtubeRegex = new Regex(@"youtu(?:\.be|be\.com)/(?:.*v(?:/|=)|(?:.*/)?)([a-zA-Z0-9-_]+)", RegexOptions.IgnoreCase);
+
+        public VideoControl(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+            InitializeComponent();
+        }
+
+        public void loadTube_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            string url = urlField.Text;
+
+            Match youtubeMatch = youtubeRegex.Match(url);
+
+            string id = string.Empty;
+
+            if (youtubeMatch.Success)
+            {
+                id = youtubeMatch.Groups[1].Value;
+                _mainWindow.youtubeViewer.Source = new Uri("https://www.youtube-nocookie.com/embed/" + id + "?rel=0&amp;iv_load_policy=3&amp;modestbranding=1&amp;autoplay=1");
+            }
+            else
+            {
+                _mainWindow.ShowMessageAsync("Error", "This doesn't appear to be a valid YouTube video URL.");
+            }
+        }
+    }
+}
---
+++ YouTube Unblocker/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+using YouTube_Unblocker.Controls;
+
+namespace YouTube_Unblocker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            videoFlyout.Content = new VideoControl(this);
+        }
+
+        public void videoButton_Click(object sender, RoutedEventArgs e) => videoFlyout.IsOpen = !videoFlyout.IsOpen;
+    }
+}
